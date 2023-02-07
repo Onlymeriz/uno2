5ba0cca84663aa46ca3e99f2d3de2dda1f37e9a0
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-02-07 14:30:33 +0700

    Update compile.sh

--- locales/compile.sh
+++ locales/compile.sh
@@ -1,39 +1,42 @@
-#!/bin/bash
-# This script compiles the unobot.po file for all languages.
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Telegram bot to play UNO in group chats
+# Copyright (c) 2016 Jannes Höke <uno@jhoeke.de>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-if [ ${PWD##*/} = "locales" ];
-then
-	function compile {
-		cd './'$1'/LC_MESSAGES/'
-		msgfmt unobot.po -o unobot.mo
-		cd ../../
-	};
-else 
-	echo 'Only execute this in the "locales" directory'
-	exit 1;
-fi
 
-# Deutsch
-compile de_DE
-# Spanish
-compile es_ES
-# Indonesian
-compile id_ID
-# Italian
-compile it_IT
-# Portuguese
-compile pt_BR
-# Russian
-compile ru_RU
-# Turkish
-compile tr_TR
-# Simplified Chinese
-compile zh_CN
-# Chinese (Honk Kong)
-compile zh_HK
-# Traditional Chinese
-compile zh_TW
-# Catalan
-compile ca_CA
-#Malayalam
-compile ml_IN
+"""Defines a dict of all available locales to the language name"""
+
+OFFSET = 127462 - ord('A')
+
+
+def flag(code):
+    return chr(ord(code[0]) + OFFSET) + chr(ord(code[1]) + OFFSET)
+
+
+available_locales = {'id_ID': flag('ID') + ' Bahasa Indonesia',
+                     'de_DE': flag('DE') + ' Deutsch (DE)',
+                     'es_ES': flag('ES') + ' Español (ES)',
+                     'en_US': flag('US') + ' English (US)',
+                     'it_IT': flag('IT') + ' Italiano',
+                     'pt_BR': flag('BR') + ' Português Brasileiro',
+                     'ru_RU': flag('RU') + ' Русский язык',
+                     'tr_TR': flag('TR') + ' Türkçe',
+                     'zh_CN': flag('CN') + ' 中文(简体)',
+                     'zh_HK': flag('HK') + ' 廣東話',
+                     'zh_TW': flag('TW') + ' 中文(台灣)',
+                     'ca_CA': flag('ES') + ' Catalan',
+                     'ml_IN': flag('IN') + ' Malayalam'}
